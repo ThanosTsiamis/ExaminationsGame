fd149ee0c9360a38b62052436b49570e849d6803
ThanosTsiamis <thanos.tsiamis@camelotls.com>
2020-09-11 19:18:18 +0300

    Added getters in Column

--- src/main/java/com/entitities/Column.java
+++ src/main/java/com/entitities/Column.java
@@ -4,6 +4,14 @@ public class Column {
     final int row;
     final int col;
 
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
     public Column(int row, int col) {
         this.row = row;
         this.col = col;
