cd661428efa4c4cabd7174c0f8ef107caedf203f
ThanosTsiamis <thanos.tsiamis@camelotls.com>
2020-09-20 12:39:22 +0300

    Created a class which creates an excel file to fix problems occured with iOS Mac Excel Program compatibility

---
+++ src/main/java/com/CreateExcel.java
@@ -0,0 +1,56 @@
+package com;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class CreateExcel {
+    public static void main(String[] args) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("Results");
+//        sheet.setColumnWidth(0, 6000);
+//        sheet.setColumnWidth(1, 4000);
+
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        for (int i = 0; i < 51; i++) {
+            Row header = sheet.createRow(i);
+            for (int j = 0; j < 14; j++) {
+                Cell headerCell = header.createCell(j);
+                headerCell.setCellValue(i + "  " + j);
+            }
+        }
+
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        String fileLocation = path.substring(0, path.length() - 1) + "Results.xlsx";
+
+        FileOutputStream outputStream = null;
+
+        outputStream = new FileOutputStream(fileLocation);
+
+        workbook.write(outputStream);
+
+        workbook.close();
+
+    }
+}
