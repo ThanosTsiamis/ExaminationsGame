8f9aaad252d40c3e42c7cd3c6755ee30c07e2644
ThanosTsiamis <thanos.tsiamis@camelotls.com>
2020-09-20 23:18:20 +0300

    Improved CreateExcel to sort of bring into a default state the excel file in case something goes wrong

--- src/main/java/com/CreateExcel.java
+++ src/main/java/com/CreateExcel.java
@@ -10,13 +10,13 @@ import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class CreateExcel {
     public static void main(String[] args) throws IOException {
-        Workbook workbook = new XSSFWorkbook();
+        XSSFWorkbook workbook = new XSSFWorkbook();
 
         Sheet sheet = workbook.createSheet("Results");
 //        sheet.setColumnWidth(0, 6000);
@@ -26,17 +26,16 @@ public class CreateExcel {
         headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
         headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        XSSFFont font = workbook.createFont();
         font.setFontName("Arial");
         font.setFontHeightInPoints((short) 16);
         font.setBold(true);
         headerStyle.setFont(font);
-
         for (int i = 0; i < 51; i++) {
             Row header = sheet.createRow(i);
             for (int j = 0; j < 14; j++) {
                 Cell headerCell = header.createCell(j);
-                headerCell.setCellValue(i + "  " + j);
+                headerCell.setCellValue(i + " xx " + j);
             }
         }
 
