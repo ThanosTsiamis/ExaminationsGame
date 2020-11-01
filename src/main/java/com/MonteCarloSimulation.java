9890e5d931441ea2bf19bc18f517dfb99832b9db
ThanosTsiamis <thanos.tsiamis@camelotls.com>
2020-11-01 19:06:01 +0200

    changed to a more appropriate name

--- src/main/java/com/MonteCarloSimulation.java
+++ src/main/java/com/MonteCarloSimulation.java
@@ -1,6 +1,6 @@
 package com;
 
-public class Run {
+public class MonteCarloSimulation {
     public static void main(String[] args) {
         for (int i = 0; i < Math.pow(10, 6); i++) {
             Main.main(args);
