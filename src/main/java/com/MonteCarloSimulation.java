3ef8db21164992358a6caa11ea265baeb27dabb2
ThanosTsiamis <thanos.tsiamis@camelotls.com>
2020-11-20 09:59:22 +0200

    More data

--- src/main/java/com/MonteCarloSimulation.java
+++ src/main/java/com/MonteCarloSimulation.java
@@ -9,9 +9,9 @@ public class MonteCarloSimulation {
         if (!file.exists()) {
             CreateExcel.main(args);
         }
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 500; i++) {
             Main.main(args);
-           // System.out.println(i); //progress bar purposes
+            System.out.println(i); //progress bar purposes
         }
     }
 }
